57113f4b6bf8fbb552669f7ff8fa58a0ef0f2224
hyeyoungkim <velvetredvelvet91@gmail.com>
2022-03-16 19:33:08 +0900

    2번째 3번째 탭 수정

--- src/Test.tsx
+++ src/Test.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import styled from '@emotion/styled'
+import Subscribe from 'components/Subscribe'
 
 export default function Test() {
   return (
     <Wrapper>
       <Tab>
-        <ul>
+        <ul >
           <li>1</li>
           <li>2</li>
           <li>3</li>
@@ -13,8 +14,7 @@ export default function Test() {
       </Tab>
       <NewPageWrapper>
         <NewPageMain>
-        <div>2</div>
-        <div>1</div>
+        <Subscribe />
 
         </NewPageMain>
       </NewPageWrapper>
@@ -47,18 +47,17 @@ const NewPageWrapper = styled.div`
 `
 
 const NewPageMain = styled.div`
-  box-shadow: rgba(67, 71, 85, 0.27) 0px 0px 0.25em, rgba(90, 125, 188, 0.05) 0px 0.25em 1em;
-  border-radius: 1rem;
-  padding: 2rem;
+
   margin: 3rem;
 `
 
 const DetailedListWrapper = styled.div`
+  box-shadow: rgba(67, 71, 85, 0.27) 0px 0px 0.25em, rgba(90, 125, 188, 0.05) 0px 0.25em 1em;
 `
 
 const DetailedMain = styled.div`
-  box-shadow: rgba(67, 71, 85, 0.27) 0px 0px 0.25em, rgba(90, 125, 188, 0.05) 0px 0.25em 1em;
+  /* box-shadow: rgba(67, 71, 85, 0.27) 0px 0px 0.25em, rgba(90, 125, 188, 0.05) 0px 0.25em 1em; */
   border-radius: 1rem;
-  padding: 2rem;
-  margin: 3rem;
+  /* padding: 2rem;
+  margin: 3rem; */
 `
